bcc60c6fcd05219b1572ba3bf9dd3ce2cd6372c7
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-06-13 17:16:37 -0300

    exercice

--- exercices/009/009.js
+++ exercices/009/009.js
@@ -2,27 +2,6 @@
 
 let programRunning = true;
 
-function CalcTriangleArea(base, height) {
-    let area = (base*height)/2;
-    return area;
-} 
-function CalcRetangleArea(base, height) {
-    let area = base*height;
-    return area;
-}
-function CalcSquareArea(side) {
-    let area = side**2;
-    return area;
-}
-function CalcTrapezoidArea(majorbase, minorbase, height) {
-    let area = ((majorbase+minorbase)*height)/2;
-    return area;
-}
-function CalcCircleArea(radius) {
-    let area = (3.14)*(radius**2);
-    return area;
-}
-
 while (programRunning) {
     let menuOptions = parseInt(prompt(`Qual área você gostaria de calcular?
     1 - Área do triângulo
@@ -33,48 +12,61 @@ while (programRunning) {
     6 - Encerrar programa.`));
 
     switch(menuOptions) {
-        // triangle
         case 1:
-            base = Number(prompt(`Insira o valor da base do triângulo:`));
-            height = Number(prompt(`Insira valor da altura do triângulo:`));
-            alert(`O triângulo de base ${base} e altura ${height} tem área igual a ${CalcTriangleArea(base, height)}`);
+            CalcTriangleArea();
+            alert(`O triângulo de base ${base} e altura ${height} tem área igual a ${area}.`);
             break
-        
-        // rectangle
         case 2: 
-            base = Number(prompt(`Insira o valor da base do retângulo:`));
-            height = Number(prompt(`Insira valor da altura do retângulo:`));
-            alert(`O retângulo de base ${base} e altura ${height} tem área igual a ${CalcRetangleArea(base, height)}`);
+            CalcRetangleArea();
+            alert(`O retângulo de base ${base} e altura ${height} tem área igual a ${area}.`);
             break
-        
-        // square
-        case 3: 
-            side = Number(prompt(`Insira o valor do lado do quadrado:`));
-            alert(`O quadrado de lado ${side} tem área igual a ${CalcSquareArea(side)}`);
+        case 3:
+            CalcSquareArea(); 
+            alert(`O quadrado de lado ${side} tem área igual a ${area}.`);
             break
-        
-        // trapezoid
         case 4:
-            majorbase = Number(prompt(`Insira o valor da base maior do trapézio:`));
-            minorbase = Number(prompt(`Insira valor da base menor do trapézio:`));
-            height = Number(prompt(`Insira valor da altura do trapézio:`));
-            alert(`O trapézio de base maior ${majorbase}, base menor ${minorbase} e altura ${height} tem área igual a ${CalcTrapezoidArea(majorbase, minorbase, height)}`);
+            CalcTrapezoidArea();
+            alert(`O trapézio de base maior ${majorbase}, base menor ${minorbase} e altura ${height} tem área igual a ${area}.`);
             break
-        
-        // circle
         case 5:
-            radius = Number(prompt(`Insira o valor do raio do círculo:`));
-            alert(`O círculo de raio ${radius} tem área igual a ${CalcCircleArea(radius)}`);
+            CalcCircleArea();
+            alert(`O círculo de raio ${radius} tem área igual a ${area}.`);
             break
-        
-        // close program
         case 6:
             programRunning = false;
             alert(`Programa encerrado.`);
             break
-
-        // validation
         default:
             alert(`[ERRO] Opção inválida. Escolha entre as opções 1 a 6.`);
+    }
+    
+    function CalcTriangleArea() {
+        base = Number(prompt(`Insira o valor da base do triângulo:`));
+        height = Number(prompt(`Insira valor da altura do triângulo:`));
+        area = (base*height)/2;
+        return area;
+    } 
+    function CalcRetangleArea() {
+        base = Number(prompt(`Insira o valor da base do retângulo:`));
+        height = Number(prompt(`Insira valor da altura do retângulo:`));
+        area = base*height;
+        return area;
+    }
+    function CalcSquareArea() {
+        side = Number(prompt(`Insira o valor do lado do quadrado:`));
+        area = side*side;
+        return area;
+    }
+    function CalcTrapezoidArea() {
+        majorbase = Number(prompt(`Insira o valor da base maior do trapézio:`));
+        minorbase = Number(prompt(`Insira valor da base menor do trapézio:`));
+        height = Number(prompt(`Insira valor da altura do trapézio:`));
+        area = ((majorbase+minorbase)*height)/2;
+        return area;
+    }
+    function CalcCircleArea() {
+        radius = Number(prompt(`Insira o valor do raio do círculo:`));
+        area = (3.14)*(radius**2);
+        return area;
     }
 }

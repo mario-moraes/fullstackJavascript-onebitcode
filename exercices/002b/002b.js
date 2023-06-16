e15df7a340d3bbd5f775863d03c410ef3cc4758a
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-06-16 19:12:56 -0300

    exercices

--- exercices/002b/002b.js
+++ exercices/002b/002b.js
@@ -1,10 +1,10 @@
 // rpg damage calculator
 
-    // first character stats:
+// first character stats:
 let firstCharName = prompt("Insira o nome do primeiro personagem");
 let firstCharDamage = Number(prompt("Insira o valor do dano do primeiro personagem"));
 
-    // second character stats:
+// second character stats:
 let secondCharName = prompt("Insira o nome do segundo personagem");
 let secondCharHealth = Number(prompt("Insira os pontos de vida do segundo personagem")); 
 let secondCharDefense = Number(prompt("Insira o valor de defesa do segundo personagem"));
@@ -12,22 +12,46 @@ let secondCharShield = prompt("O segundo personagem possui um escudo? Digite sim
 
 let log = document.querySelector("div#log");
 
-    // damage calc:
+// damage calc:
 if (firstCharDamage <= secondCharDefense) {
     
-    log.innerHTML = `<p>- Stats personagem 1<br>Nome: ${firstCharName}<br>Dano: ${firstCharDamage}</p><p>- Stats personagem 2<br>Nome: ${secondCharName}<br> Vida: ${secondCharHealth}<br> Defesa: ${secondCharDefense}<br> Escudo: ${secondCharShield}</p><p>Logo, ${firstCharName} não causou nenhum dano em ${secondCharName}, o deixando com seus ${secondCharHealth} pontos de vida iniciais.</p>`;
+    alert(`- Stats personagem 1
+    Nome: ${firstCharName}.
+    Dano: ${firstCharDamage}.
+    - Stats personagem 2
+    Nome: ${secondCharName}.
+    Vida: ${secondCharHealth}.
+    Defesa: ${secondCharDefense}.
+    Escudo: ${secondCharShield}.
+    Logo, ${firstCharName} não causou nenhum dano em ${secondCharName}, o deixando com seus ${secondCharHealth} pontos de vida iniciais.`);
 }
 
 if (firstCharDamage > secondCharDefense && secondCharShield === "nao") {
     let damageCaused = firstCharDamage - secondCharDefense;
     let finalHealth = secondCharHealth - damageCaused;
     
-    log.innerHTML = `<p>- Stats personagem 1<br>Nome: ${firstCharName}<br>Dano: ${firstCharDamage}</p><p>- Stats personagem 2<br>Nome: ${secondCharName}<br> Vida: ${secondCharHealth}<br> Defesa: ${secondCharDefense}<br> Escudo: ${secondCharShield}</p><p>Logo, ${firstCharName} causou um dano de ${damageCaused} em ${secondCharName}, o deixando com ${finalHealth} pontos de vida.</p>`;
+    alert(`- Stats personagem 1
+    Nome: ${firstCharName}.
+    Dano: ${firstCharDamage}.
+    - Stats personagem 2
+    Nome: ${secondCharName}.
+    Vida: ${secondCharHealth}.
+    Defesa: ${secondCharDefense}.
+    Escudo: ${secondCharShield}.
+    Logo, ${firstCharName} causou um dano de ${damageCaused} em ${secondCharName}, o deixando com ${finalHealth} pontos de vida.`);
 }
 
 if (firstCharDamage > secondCharDefense && secondCharShield === "sim") {
     let damageCaused = (firstCharDamage - secondCharDefense) / 2;
     let finalHealth = secondCharHealth - damageCaused;
 
-    log.innerHTML = `<p>- Stats personagem 1<br>Nome: ${firstCharName}<br>Dano: ${firstCharDamage}</p><p>- Stats personagem 2<br>Nome: ${secondCharName}<br> Vida: ${secondCharHealth}<br> Defesa: ${secondCharDefense}<br> Escudo: ${secondCharShield}</p><p>Logo, ${firstCharName} causou um dano de ${damageCaused} em ${secondCharName}, o deixando com ${finalHealth} pontos de vida.</p>`
+    alert(`- Stats personagem 1
+    Nome: ${firstCharName}.
+    Dano: ${firstCharDamage}
+    - Stats personagem 2
+    Nome: ${secondCharName}.
+    Vida: ${secondCharHealth}.
+    Defesa: ${secondCharDefense}.
+    Escudo: ${secondCharShield}.
+    Logo, ${firstCharName} causou um dano de ${damageCaused} em ${secondCharName}, o deixando com ${finalHealth} pontos de vida.`);
 }

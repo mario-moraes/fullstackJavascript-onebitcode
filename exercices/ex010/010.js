924229f71022daf6adb9d9386c93a5c61874257b
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-06-19 18:40:15 -0300

    exercices

--- exercices/ex010/010.js
+++ exercices/ex010/010.js
@@ -2,6 +2,9 @@
 
 let programRunning = true; 
 
+let jobInfoList = [];
+let jobList = [];  
+
 while (programRunning) {
     let menuOptions = Number(prompt(`Selecione uma das opçöes a seguir:
     1. Listar vagas disponíveis
@@ -10,24 +13,53 @@ while (programRunning) {
     4. Inscrever um candidato em uma vaga
     5. Excluir uma vaga
     6. Sair`));
+
     switch(menuOptions) {
-        case 1: 
+        case 1: showJobOfferList();
             break;
         
-        case 2:
+        case 2: createJobOffer();
             break;
         
-        case 3:
+        case 3: visualizeJobOffer();
             break;
         
-        case 4:
+        case 4: applyJobOffer();
             break;
         
-        case 5:
+        case 5: deleteJobOffer();
             break;
         
         case 6:
+            programRunning = false;
+            alert(`Programa encerrado.`);
             break;
+        
+        default:
+            alert(`[ERRO] Opção inválida, escolha entre as opções 1 a 6.`);
     }
+    
+    function showJobOfferList() {};
+    
+    function createJobOffer(){
+        let jobName = prompt(`Insira o nome da vaga:`);
+        let jobDescription = prompt(`Insira a descrição da vaga:`);
+        let jobLimitDate = prompt(`Insira a data limite para aplicação da vaga:`);
+        
+        let jobInfo = { 
+            name: jobName,
+            description: jobDescription,
+            limit: jobLimitDate 
+        }
+        jobInfoList.push(jobInfo);
+        alert(`Dados inseridos com sucesso!
+        - Nome da vaga:${jobInfo.name}
+        - Descrição: ${jobInfo.description}
+        - Data limite: ${jobInfo.limit}`);
+    };
+    
+    function visualizeJobOffer(){};
+    function applyJobOffer(){};
+    function deleteJobOffer(){};
 
 }

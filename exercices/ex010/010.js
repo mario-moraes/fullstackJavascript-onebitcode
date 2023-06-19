18dc1e8d25a68982fa1677b2197d5f588cc6c077
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-06-19 18:12:59 -0300

    exercices

---
+++ exercices/ex010/010.js
@@ -0,0 +1,33 @@
+// vacancy jobs program
+
+let programRunning = true; 
+
+while (programRunning) {
+    let menuOptions = Number(prompt(`Selecione uma das opçöes a seguir:
+    1. Listar vagas disponíveis
+    2. Criar um nova vaga
+    3. Visualizar uma vaga
+    4. Inscrever um candidato em uma vaga
+    5. Excluir uma vaga
+    6. Sair`));
+    switch(menuOptions) {
+        case 1: 
+            break;
+        
+        case 2:
+            break;
+        
+        case 3:
+            break;
+        
+        case 4:
+            break;
+        
+        case 5:
+            break;
+        
+        case 6:
+            break;
+    }
+
+}

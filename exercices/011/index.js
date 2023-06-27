971c0f421397000caf434f79fc855fddf567d152
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-06-27 17:19:23 -0300

    DOM exercice

--- exercices/011/index.js
+++ exercices/011/index.js
@@ -1,8 +1,6 @@
 // select soccer team players (DOM exercice)
 
-const team = [];
-
-function PickPlayer() {
+function pickPlayer() {
     const playerName = document.querySelector(`#playerName`).value;
     const playerPosition = document.querySelector(`#playerPosition`).value;
     const playerNumber = document.querySelector(`#playerNumber`).value;
@@ -15,7 +13,26 @@ function PickPlayer() {
             playerPosition,
             playerNumber
         }
-        team.push(playerInfo);
-    }
 
+        const playersUl = document.querySelector(`#playersUl`);
+
+        const li = document.createElement(`li`);
+        
+        li.innerText = `Jogador: ${playerInfo.playerName} / Posição: ${playerInfo.playerPosition} / Camisa: ${playerInfo.playerNumber}.`;
+
+        playersUl.appendChild(li);
+
+        document.querySelector(`#playerName`).value = '';
+        document.querySelector(`#playerPosition`).value = '';
+        document.querySelector(`#playerNumber`).value = '';
+    }
 }
+function removePlayer() {
+    const playerNumber = document.querySelector(`#playerNumber`).value;
+    const confirmation = confirm(`Você gostaria de remover o jogador camisa ${playerNumber}?`);
+
+    if(confirmation) {
+    }
+}
+
+

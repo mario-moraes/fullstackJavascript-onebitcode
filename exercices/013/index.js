d350f5c4c3f4f23887e5d2cee1bac287d99b0631
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-07-11 16:54:55 -0300

    DOM exercice: tic tact toe

--- exercices/013/index.js
+++ exercices/013/index.js
@@ -2,12 +2,15 @@ let player = ``;
 let virtualBoard = [];
 
 function beginGame() {
-  player = `playerOne`;
+  player = `player-one`;
   virtualBoard = [['', '', ''], ['', '', ''], ['', '', '']];
+  document.querySelector('h2').innerHTML = 'Player Turn:<span id="players-turn"></span>';
   updateTitle();
   const boardSpans = document.querySelectorAll(`#board span`);
   boardSpans.forEach(function(span){
+    span.classList.remove('win');
     span.innerText = ``;
+    span.classList.add('cursor-pointer');
     span.addEventListener(`click`, handleRegionClick)
   });
 };
@@ -23,23 +26,24 @@ function handleRegionClick(e){
   const rowColumnPair = region.split('.');
   const row = rowColumnPair[0];
   const column = rowColumnPair[1];
-  if(player === `playerOne`) {
+  if(player === `player-one`) {
     span.innerText = `X`;
     virtualBoard[row][column] = 'X';
   } else {
     span.innerText = `O`;
     virtualBoard[row][column] = 'O';
   }
+  
   disableRegion(span);
 
   const winRegions = getWinRegions();
   if (winRegions.length > 0) {
     handleWin(winRegions);
   } else if (virtualBoard.flat().includes('')) {
-    player = player === 'playerOne' ? 'playerTwo' : 'playerOne';
+    player = player === 'player-one' ? 'player-two' : 'player-one';
     updateTitle();
   } else {
-    document.querySelector('#players-turn').innerHTML = 'Empate!';
+    document.querySelector(`h2`).innerHTML = 'Empate!';
   }
 };
 
@@ -67,7 +71,7 @@ function getWinRegions() {
   if (virtualBoard[2][0] && virtualBoard[2][0] === virtualBoard[2][1] && virtualBoard[2][0] === virtualBoard[2][2])
     winRegions.push("2.0", "2.1", "2.2");
   
-  if (virtualBoard[0][0] && vBoard[0][0] === virtualBoard[1][0] && virtualBoard[0][0] === virtualBoard[2][0])
+  if (virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[1][0] && virtualBoard[0][0] === virtualBoard[2][0])
     winRegions.push("0.0", "1.0", "2.0");
   
   if (virtualBoard[0][1] && virtualBoard[0][1] === virtualBoard[1][1] && virtualBoard[0][1] === virtualBoard[2][1])
@@ -79,7 +83,7 @@ function getWinRegions() {
   if (virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[1][1] && virtualBoard[0][0] === virtualBoard[2][2])
     winRegions.push("0.0", "1.1", "2.2");
   
-  if (virtualBoard[0][2] && vBoard[0][2] === virtualBoard[1][1] && virtualBoard[0][2] === virtualBoard[2][0])
+  if (virtualBoard[0][2] && virtualBoard[0][2] === virtualBoard[1][1] && virtualBoard[0][2] === virtualBoard[2][0])
     winRegions.push("0.2", "1.1", "2.0");
   return winRegions
 };

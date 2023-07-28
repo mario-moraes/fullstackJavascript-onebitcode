66f864fcc0ed7601126887eec711c21738291c1a
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-07-28 20:29:21 -0300

    feature: added a simplistic login auth

--- exercices/018/User.js
+++ exercices/018/User.js
@@ -1,1 +1,19 @@
 // OOP fundamentals exercice
+class User {
+  constructor(fullname, email, password) {
+    this.fullname = fullname,
+    this.email = email,
+    this.password = password
+  }
+  login(email, password) {
+    if (email === this.email && password === this.password) {
+      console.log(`Login bem sucedido!`);
+    } else {
+      console.log(`Erro: email ou senha não condizem com este usuario.`);
+    }
+  }
+}
+
+const user1 = new User("Mario Moraes", "mario@email.com", "1234");
+
+user1.login("mario@email.com", "1234");

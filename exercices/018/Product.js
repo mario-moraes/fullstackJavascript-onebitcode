715551e5786e022ed69a48d26a17ee73b91dcec5
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-07-28 20:43:37 -0300

    feature: added discount calculator to stock object

--- exercices/018/Product.js
+++ exercices/018/Product.js
@@ -1,1 +1,23 @@
 // OOP fundamentals exercice
+class Product {
+  constructor(name, description, price) {
+    this.name = name,
+    this.description = description,
+    this.price = price,
+    this.inStock = 0
+  }
+  addToStock(quantity){
+    this.inStock += quantity
+  }
+  calculateDiscount(percentage) {
+    const discountValue = this.price * (percentage / 100);
+    const finalPrice = this.price - discountValue;
+    return finalPrice
+  }
+}
+
+const xbox = new Product("Xbox Series S", "Gaming Console", 250);
+
+xbox.addToStock(5);
+
+console.log(xbox.calculateDiscount(50));

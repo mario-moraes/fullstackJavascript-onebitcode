56850440c99b1b7ce01c9aa32c71cec7f45de6b4
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-08-06 16:17:42 -0300

    ex 022: created Account and User classes

---
+++ exercices/022/Account.js
@@ -0,0 +1,35 @@
+module.exports = class Account {
+  #balance;
+  //#deposits
+  //#loans
+  //#transfers 
+  // values are public for exercice purposes, but in real-world applications be careful to set to private atributes
+  constructor(user) {
+    this.owner = user;
+    this.#balance = 0;
+    this.deposits = [];
+    this.loans = [];
+    this.transfers = [];
+  }
+
+  get balance() {
+    return this.#balance
+  }
+  addDeposit(deposit) {
+    this.#balance =+ deposit.value;
+    this.deposits.push(deposit);
+  }
+  addLoan(loan) {
+    this.#balance += loan.value;
+    this.loans.push(loan);
+  }
+  addTransfer(transfer) {
+    if (transfer.toUser.email === this.owner.email) {
+      this.#balance += transfer.value;
+      this.transfers.push(transfer);
+    } else if (transfer.fromUser.email === this.owner.email) {
+      this.#balance -= transfer.value;
+      this.transfers.push(transfer);
+    }
+  }
+}

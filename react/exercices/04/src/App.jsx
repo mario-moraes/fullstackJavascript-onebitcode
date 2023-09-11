8ab47c46778e48b3b4485c44a6c0f8dc9d256f5b
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-09-11 10:41:35 -0300

    react exercice(password generator): feature generate

--- react/exercices/04/src/App.jsx
+++ react/exercices/04/src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react"
+
 function App() {
-  
+  const [password, setPassword] = useState("");
+
+  function generatePassword() {
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
+    const length = 12;
+    let newPassword = "";
+    
+    for (let i = 0; i < length; i++) {
+      const randomPosition = Math.floor(Math.random() * characters.length);
+      newPassword += characters[randomPosition];
+    }
+    
+    setPassword(newPassword);
+  }
+
   return (
     <div className="app">
       <h1>Password generator</h1>
-      <button>Generate</button>
+      <button onClick={generatePassword}>Generate</button>
       <button>Copy</button>
-      <div>password</div>
+      <div>{password}</div>
     </div>
   )
 }

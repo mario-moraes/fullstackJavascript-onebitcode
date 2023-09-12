f905ea1c348b50162f0870d6af18a4e48d6afdb0
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-09-12 16:48:07 -0300

    project game library (react practice): feature - add and remove games

--- react/project-game-library/src/App.jsx
+++ react/project-game-library/src/App.jsx
@@ -1,7 +1,68 @@
+import { useState } from "react";
+
 export default function App() {
+
+  const [games, setGames] = useState([]);
+  const [title, setTitle] = useState("");
+  const [cover, setCover] = useState("");
+  
+  const addGame = ({title, cover}) => {
+    const id = Math.floor(Math.random() * 1000000);
+    const game = { id, title, cover};
+    setGames((state) => [...state, game]); 
+  };
+  
+  const removeGame = (id) => {
+    setGames((state) => state.filter((game) => game.id !== id))
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addGame({title, cover});
+    setTitle("");
+    setCover(""); 
+  };
+
   return (
     <div id="app">
       <h1>Game Library</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)} 
+          />
+        </div>
+        <div>
+          <label htmlFor="cover">Cover</label>
+          <input
+            type="text"
+            name="cover"
+            id="cover"
+            value={cover}
+            onChange={(e) => setCover(e.target.value)} 
+          />
+          </div>
+          <button type="submit">Add to library</button>
+      </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id}>
+            <img src={game.cover} alt="game boxart" />
+            <div>
+              <h2>{game.title}</h2>
+              <button 
+                onClick={() => removeGame(game.id)}>
+                  Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }

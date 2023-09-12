562dd0f3e7ab9f19d9d6e511293c709df9fc7c78
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-09-12 10:36:00 -0300

    react exercice(password generator): feature - length input

--- react/exercices/04/src/App.jsx
+++ react/exercices/04/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react"
 
 function App() {
   const [password, setPassword] = useState("");
-  const [copyText, setCopyText] = useState ("Copy"); 
+  const [copyText, setCopyText] = useState ("Copy");
+  const [passwordSize, setPasswordSize] = useState(12);
 
   function generatePassword() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
-    const length = 12;
     let newPassword = "";
     
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const randomPosition = Math.floor(Math.random() * characters.length);
       newPassword += characters[randomPosition];
     }
@@ -26,9 +26,26 @@ function App() {
   return (
     <div className="app">
       <h1>Password generator</h1>
-      <button onClick={generatePassword}>Generate</button>
-      <button onClick={copyToClipboard}>{copyText}</button>
-      <div>{password}</div>
+      <div>
+        <label 
+          htmlFor="passwordSize">Password length:</label>
+        <input 
+          type="number" 
+          id="passwordSize" 
+          min={1}
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)}
+        />
+      </div>
+      <button
+        onClick={generatePassword}>Generate a password with {passwordSize} characters
+      </button>
+      <button 
+        onClick={copyToClipboard}>{copyText}
+      </button>
+      <div>
+        {password}
+      </div>
     </div>
   )
 }

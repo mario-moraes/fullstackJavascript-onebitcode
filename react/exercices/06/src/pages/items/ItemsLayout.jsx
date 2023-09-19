e52e3f8910620ce936ac4fa002f71ff85382cc4b
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-09-19 10:58:52 -0300

    react(final exercice - stock manager): altering active class for items through useLocation() on ItemsLayout.jsx

--- react/exercices/06/src/pages/items/ItemsLayout.jsx
+++ react/exercices/06/src/pages/items/ItemsLayout.jsx
@@ -1,9 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 export default function ItemsLayout() {
+  const { pathname } = useLocation();
+
   return (
     <main>
       <h1>Stock Items</h1>
+      <div className="tabs">
+        <Link 
+          to="/items/" 
+          className={`tab ${pathname === "/items/" ? "active" : ""}`}
+        >All items</Link>
+        <Link 
+          to="/items/new" 
+          className={`tab ${pathname === "/items/new" ? "active" : ""}`}
+        >New item</Link>
+      </div>
       <Outlet />
     </main>
   )

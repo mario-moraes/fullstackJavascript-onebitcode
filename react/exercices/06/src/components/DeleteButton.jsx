b63c2ad5d54828936cc0c3c7165b3364bb0696da
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-09-25 11:28:08 -0300

    react(final exercice - stock manager): fix - correctly redirected user after deleting an item on check ou item page

--- react/exercices/06/src/components/DeleteButton.jsx
+++ react/exercices/06/src/components/DeleteButton.jsx
@@ -1,12 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import useStock from "../hooks/useStock"
 
 export default function DeleteButton({ itemId, itemName }) {
   
   const { deleteItem } = useStock();
-  
+  const navigate = useNavigate()
+
   const handleDelete = () => {
     if (confirm(`Are you sure you want to delete "${ itemName }"?`)) {
       deleteItem(itemId)
+      navigate("/items")
     }
   }
   

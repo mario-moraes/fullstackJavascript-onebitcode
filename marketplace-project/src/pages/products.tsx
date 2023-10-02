51824fc66b39d6d0027565f5ffe3d556647c452c
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-10-02 15:24:12 -0300

    marketplace-project(nextjs): creatig and consuming product API

--- marketplace-project/src/pages/products.tsx
+++ marketplace-project/src/pages/products.tsx
@@ -1,22 +1,43 @@
 // pages/products.tsx
 
-import Header from "@/components/Header";
-import { NextPage } from "next";
-import Head from "next/head";
+import { GetStaticProps, NextPage } from 'next'
+import Head from 'next/head'
+import { ReactNode } from 'react'
+import { Container } from 'reactstrap'
 
-const Products: NextPage = () => {
+
+import { fetchProducts, ProductType } from '../services/products'
+import Header from '@/components/Header'
+import ProductsList from '@/components/ProductsList'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await fetchProducts()
+  return { props: { products } }
+}
+
+const Products: NextPage = (props: {
+  children?: ReactNode
+  products?: ProductType[]
+}) => {
   return (
     <>
       <Head>
         <title>Our products</title>
-        <meta name="description" content="Get to know more details about our product line!" />
+        <meta name="description" content="Get to know the details about the product" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <h1>
-        Our products
-      </h1>
+
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">
+            Our Products
+          </h1>
+
+          {<ProductsList products={props.products!} />}
+        </Container>
+      </main>
     </>
   )
 }

8ca30dffc7fb9407aeab67c0afabe00ed4d3c569
mario-moraes <129066014+mario-moraes@users.noreply.github.com>
2023-10-02 17:02:57 -0300

    marketplace-project(nextjs): feature - shopping cart

--- marketplace-project/src/pages/cart.tsx
+++ marketplace-project/src/pages/cart.tsx
@@ -3,6 +3,9 @@
 import Header from "@/components/Header";
 import { NextPage } from "next";
 import Head from "next/head";
+import CartTable from "../../src/components/CartTable";
+import { Container } from "reactstrap";
+import CartTotal from "../../src/components/CartTotal";
 
 const Cart: NextPage = () => {
   return (
@@ -15,9 +18,16 @@ const Cart: NextPage = () => {
 
       < Header />
 
-      <h1>
-        Shopping Cart
-      </h1>
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">
+            Shopping Cart
+          </h1>
+
+          <CartTable />
+          <CartTotal />
+        </Container>
+      </main>
     </>
   )
 }
